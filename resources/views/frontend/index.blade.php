e0001220663fbcfa53f41a4baa8e87a2b0641509
João <tccjvga@gmail.com>
2022-06-30 23:35:23 -0300

    segundo_commit

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,10 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-
-<!--primeiro_mod00 -->
-
-
 <head>
     <meta charset="utf-8">
     <title>{{ config('app.name', 'OnVet') }}</title>
